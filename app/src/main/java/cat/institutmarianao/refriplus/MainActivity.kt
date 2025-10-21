61845038176f7d7635117b8cb125b2cb5e2b70f5
ItsNikai <invernon52@gmail.com>
2025-10-21 20:28:58 +0200

    actualización del layout + error por campo vacio

--- app/src/main/java/cat/institutmarianao/refriplus/MainActivity.kt
+++ app/src/main/java/cat/institutmarianao/refriplus/MainActivity.kt
@@ -1,16 +1,34 @@
 package cat.institutmarianao.refriplus
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val emailLayout = findViewById<TextInputLayout>(R.id.emailLayout)
+        val emailText = findViewById<TextInputEditText>(R.id.emailLayout)
+        val passwordLayout = findViewById<TextInputLayout>(R.id.passwordLayout)
+        val passwordText = findViewById<TextInputEditText>(R.id.passwordLayout)
+        val loginButton = findViewById<Button>(R.id.login)
+
+
+        loginButton.setOnClickListener {
+
+            if (emailText.text.isNullOrEmpty()) {
+                emailLayout.error = "Email is required"
+            }
+
+            if (passwordText.text.isNullOrEmpty()) {
+                passwordLayout.error = "Password is required"
+            }
+        }
 
     }
 }
